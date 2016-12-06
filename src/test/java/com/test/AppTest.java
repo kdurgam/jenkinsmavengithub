fed7276252a8d3b5424571276b8ffeaf6d916f5a
Khasim Peera Durgam <Khasim Peera Durgam>
2016-12-06 10:00:30 +0530

    first commit

---
+++ src/test/java/com/test/AppTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void SampleTest()
+	{
+		System.out.println("ant ant ant ant ant ant ant");
+	}
+
+}
